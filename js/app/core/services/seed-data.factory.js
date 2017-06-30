ddd3ed94f176998b838899aa90d34b4f36e4dc98
Mdjurdj1 <mdjurdj1@gmail.com>
2017-06-30 01:53:45 -0400

    Added code to open/close modals

--- js/app/core/services/seed-data.factory.js
+++ js/app/core/services/seed-data.factory.js
@@ -1,3 +1,5 @@
+// This is a seed data factory service to be used with angular resource. Currently unused.
+
 angular.module('app').factory('SeedDataFactory', function ($resource) {
 
     return $resource('core/data/:filename.json', {
--- js/app/modules/menu/controllers/menu.controller.js
+++ js/app/modules/menu/controllers/menu.controller.js
@@ -2,13 +2,39 @@ angular.module('angularRestaurant.menu').controller('MenuController',
   function ($uibModal) {
 
     var vm = this;
-    vm.data = "heh"
+    vm.lunch_data = [ {
+      "name": "Mini Chicken Quesadillas",
+      "description": "2 mini chicken Quesadillas served with sour cream & salsa.",
+      "price": "$6.95"
+    }, {
+      "name": "Boneless Chicken Crispers",
+      "description": "6 pieces with your choice of breaded, Naked, or Grilled with rosemary, onions & sauce.",
+      "price": "$8.25"
+    }, {
+      "name": "Empanadas (2)",
+      "description": "Deep fried Empanadas stuffed with your choice of beef or cheese",
+      "price": "$5.95"
+    } ]
 
-    vm.open = function (item) {
+
+    vm.open_lunch = function (item) {
       $uibModal.open({
         templateUrl: 'js/app/modules/menu/templates/modal.html',
         controller: 'ModalController as modal',
-        size: 'md',
+        size: 'lg',
+        resolve: {
+          itemResolve: function () {
+            return item;
+          }
+        }
+      });
+    };
+
+    vm.open_dinner = function (item) {
+      $uibModal.open({
+        templateUrl: 'js/app/modules/menu/templates/modal.html',
+        controller: 'ModalController as modal',
+        size: 'lg',
         resolve: {
           itemResolve: function () {
             return item;
