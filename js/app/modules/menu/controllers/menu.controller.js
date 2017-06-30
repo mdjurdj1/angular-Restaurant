76e3dd6471dc18e18e30551251c9305e6d123afe
Mdjurdj1 <mdjurdj1@gmail.com>
2017-06-30 02:52:57 -0400

    item resolves now pass menu data from view to the controller

--- js/app/modules/menu/controllers/menu.controller.js
+++ js/app/modules/menu/controllers/menu.controller.js
@@ -2,20 +2,6 @@ angular.module('angularRestaurant.menu').controller('MenuController',
   function ($uibModal) {
 
     var vm = this;
-    vm.lunch_data = [ {
-      "name": "Mini Chicken Quesadillas",
-      "description": "2 mini chicken Quesadillas served with sour cream & salsa.",
-      "price": "$6.95"
-    }, {
-      "name": "Boneless Chicken Crispers",
-      "description": "6 pieces with your choice of breaded, Naked, or Grilled with rosemary, onions & sauce.",
-      "price": "$8.25"
-    }, {
-      "name": "Empanadas (2)",
-      "description": "Deep fried Empanadas stuffed with your choice of beef or cheese",
-      "price": "$5.95"
-    } ]
-
 
     vm.open_lunch = function (item) {
       $uibModal.open({
--- js/app/modules/menu/controllers/modal.controller.js
+++ js/app/modules/menu/controllers/modal.controller.js
@@ -1,8 +1,30 @@
 angular.module('angularRestaurant.menu').controller('ModalController', function ($uibModalInstance, itemResolve) {
 
   var vm = this;
+  vm.item = itemResolve
 
-  vm.item = itemResolve;
+  vm.lunch_data = [ {
+    "name": "Mini Chicken Quesadillas",
+    "image": "http://ot-foodspotting-production.s3.amazonaws.com/reviews/2889450/thumb_275.jpg?1356144144",
+    "description": "2 mini chicken Quesadillas served with sour cream & salsa.",
+    "price": "$6.95"
+  }, {
+    "name": "Boneless Chicken Crispers",
+    "image": "https://spicysouthernkitchen.com/wp-content/uploads/chicken-crispers-3-300x300.jpg",
+    "description": "6 pieces with your choice of breaded, Naked, or Grilled with rosemary, onions & sauce.",
+    "price": "$8.25"
+  }, {
+    "name": "Empanadas (2)",
+    "image": "http://cookdiary.net/wp-content/uploads/images/Empanadas_5538.jpg",
+    "description": "Deep fried Empanadas stuffed with your choice of beef or cheese",
+    "price": "$5.95"
+  }, {
+    "name": "Bacon Cheeseburger and Fries",
+    "image": "https://m.texasroadhouse.com/sf-images/default-source/mobile-app-menu/burgers-and-sandwiches/bacon-cheeseburger.jpg?sfvrsn=2",
+    "description": "A delicious burger topped with carmelized onions, applewood bacon, and a side of fresh french fries!",
+    "price": "$5.95"
+  }
+ ]
 
   vm.close = function () {
     $uibModalInstance.close();
